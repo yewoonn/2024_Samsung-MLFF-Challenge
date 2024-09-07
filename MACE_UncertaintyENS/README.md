f18f2c1ee730c61627107fab7ec1ac50d752793e
yewoonn <hanyw0207@naver.com>
2024-09-08 06:53:16 +0900

    MACE_UncertaintyENS

---
+++ MACE_UncertaintyENS/README.md
@@ -0,0 +1,36 @@
+# MACE_UncertaintyENS
+
+### Train
+- 일반 MACE 모델
+
+### Evaluate
+- Energy&Force(EF) : 체크포인트 리스트의 첫번째 모델로 Energy와 Force 예측
+- Uncertainty : 체크포인트 리스트의 모든 모델의 에너지의 표준편차로 uncertainty 계산
+(/src/md_evaluate/base_evaluator.py의 self.ckpt_list에 체크포인트 추가)
+
+### 제출 결과
+None
+
+    |ID|EF-metric|OOD|
+    |--|---------|---|
+
+
+### 명령어
+1. Train : ./run_train.sh [GPU] MACE HfO
+    ./run_train.sh 0 MACE HfO
+   - HfO 파일 복붙해서 추가하기
+
+2. Evaluate : ./run_evaluate_ef.sh [GPU] [체크 포인트 경로] [Leaderboard 데이터 경로]
+    ./run_evaluate_ef.sh 0 ../../train_results/HfO/MACE/checkpoints/train-20240908_062602/best_checkpoint.pt ../../datasets/HfO/leaderboard.xyz
+   - 모델 훈련 폴더와 예측 폴더 동일하게 유지 (shift_taps도 바뀌면 안됨)
+   - base_evaluator.py의 ckpt_list에 체크포인트 리스트 추가
+     (체크 포인트마다 모델 종류가 다를 경우, 오류 발생)
+
+### 수정 파일
+1. Train
+None
+
+2. Evaluate
+    - /src/md_evaluate/base_evaluator.py : 체크포인트 리스트로 매개 변수 수정
+    - /src/md_evaluate/calculator.py : __init__(), convert_atoms_to_batch(), caculate() 함수 수정, uncertainty 예측 추가
+    - /src/md_evaluate/energy_force_evaluator.py : csv 저장 및 출력 추가
---
+++ MACE_UncertaintyENS/src/md_evaluate/base_evaluator.py
@@ -0,0 +1,62 @@
+"""
+Copyright (C) 2023 Samsung Electronics Co. LTD
+
+This software is a property of Samsung Electronics.
+No part of this software, either material or conceptual may be copied or distributed, transmitted,
+transcribed, stored in a retrieval system or translated into any human or computer language in any form by any means,
+electronic, mechanical, manual or otherwise, or disclosed
+to third parties without the express written permission of Samsung Electronics.
+"""
+
+import os
+import torch
+import errno
+
+from src.common.registry import md_evaluate_registry
+from src.common.utils import bm_logging
+from src.md_evaluate.calculator import BenchmarkCalculator
+
+
+@md_evaluate_registry.register_md_evaluate("base_evaluator")
+class BaseEvaluator:
+    def __init__(self, config):
+        if config.get("device"):
+            self.device = torch.device(config["device"])
+        else:
+            if torch.cuda.is_available():
+                self.device = torch.device("cuda")
+            else:
+                self.device = torch.device("cpu")
+
+        ### 고정된 체크포인트 경로를 상수로 지정
+        self.ckpt_list = [
+            "/data1/project/yewon/Samsung_MLFF_Models/MACE_AllENS/train_results/HfO/MACE/checkpoints/train-20240908_062602/best_checkpoint.pt",
+            "/data1/project/yewon/Samsung_MLFF_Models/MACE_AllENS/train_results/HfO/MACE/checkpoints/train-20240908_062602/checkpoint.pt",
+        ]
+
+        # load a config which was used to train the model
+        self.config = config
+
+        # load a checkpoint
+        # checkpoint_path = self.config.get("checkpoint", None)
+
+        # if checkpoint_path is not None:
+        #     if not os.path.isfile(checkpoint_path):
+        #         raise FileNotFoundError(errno.ENOENT, "Checkpoint file not found", checkpoint_path)
+        #     ckpt = torch.load(checkpoint_path, map_location="cpu")
+
+            # set a calculator using the loaded model
+        self.calculator = BenchmarkCalculator(
+            ckpt_list=self.ckpt_list,
+            device=self.device,
+        )
+            
+        self.logger = bm_logging
+
+    def evaluate(self):
+        """Derived evaluator classes should implement this function."""
+        pass
+
+    def simulate(self):
+        """Simulator which is derived from BaseEvaluator should implement this function."""
+        pass
